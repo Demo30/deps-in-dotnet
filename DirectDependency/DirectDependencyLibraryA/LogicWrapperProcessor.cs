9a106274eea4a3c3109e1a267b7e208fc82e46a3
Demo30 <wiesnertoma@gmail.com>
2025-01-16 12:32:02 +0100

    scenario

---
+++ DirectDependency/DirectDependencyLibraryA/LogicWrapperProcessor.cs
@@ -0,0 +1,22 @@
+﻿using TransitiveDependency;
+
+namespace DirectDependency;
+
+public class LogicWrapperProcessor
+{
+    private readonly ISomeLogicProcessor _someLogicProcessor = new SomeLogicProcessor();
+
+    public string Compute(string input)
+    {
+        var result = _someLogicProcessor.Calculate(input).Result;
+
+        var expectedBehavior = $"{input} - {"Transitive dependency v1"}";
+        
+        if (result != expectedBehavior)
+        {
+            throw new Exception($"Expected behavior {expectedBehavior}, but was {result}");
+        }
+        
+        return result;
+    }
+}
